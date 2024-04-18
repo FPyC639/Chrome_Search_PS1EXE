567b744a28ac1ef3b3e6e4efff5edc72fe308b0c
Jose M Serra Jr <joseserra803@gmail.com>
2024-04-18 19:39:17 -0400

    Added init files.

---
+++ ChromePS.ps1
@@ -0,0 +1,46 @@
+﻿# Load the necessary assembly for Windows Forms
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create the main form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Google GUI'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+# Create a label
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "Enter your command:"
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$form.Controls.Add($label)
+
+# Create a text box for input
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+# Create a submit button
+$submitButton = New-Object System.Windows.Forms.Button
+$submitButton.Location = New-Object System.Drawing.Point(10,70)
+$submitButton.Size = New-Object System.Drawing.Size(75,23)
+$submitButton.Text = 'Submit'
+$form.Controls.Add($submitButton)
+
+# Action for submit button click
+$submitButton.Add_Click({
+    try {
+        # Execute the command from the text box
+        & "C:\Program Files\Google\Chrome\Application\chrome.exe" $textBox.Text
+    } catch {
+        [System.Windows.Forms.MessageBox]::Show("Error executing command: $_", "Error")
+    }
+    # Clear the text box after command execution
+    $textBox.Text = ''
+})
+
+# Add Form Closing event handler
+
+
+# Show the form
+$form.ShowDialog()
---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Chrome_Search_PS1EXE
+
+In this PowerShell application using Windows Form I was able to generate a Graphical User Interface that when inputted urls search for them in Google Chrome browser.
+
+For example:
+
+## Test Values
+
+https://www.bing.com
+https://www.google.com
+https://www.bing.com/search?q="America Short hair cats"
+https://www.google.com/search?q="Puss in Boots"
+
+These URLs allow you navigate to the websites you desire.
+
+Additional desire would be to create a search CLI inteface that allows one to input the query into a search engine of choice allowing users to choose the website of choice.
+
+## Acknowledgements
+
+ - Professors at Kean University
+ - Professors at NJIT
+ - Research Mentors at Kean University
+ - StackOverflow Q&A Discussion
+ - ChatGPT
+ - ClaudeAI
+
+
+## Authors
+
+- [@FPyC639](https://github.com/FPyC639)
+
+
+## Appendix
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/joseserra8x)
